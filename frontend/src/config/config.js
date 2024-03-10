9940321cdfecd9da11ea6271b3a7f8dcaa3c94cb
techsmit96 <sumitsakpal02@gmail.com>
2024-03-10 19:45:14 +0530

    changes

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:5000/api/";
-// export const API_URL = "https://sumitsakpal.onrender.com/api/";
+// export const API_URL = "http://localhost:5000/api/";
+export const API_URL = "https://akshadakadam.onrender.com/api/";
